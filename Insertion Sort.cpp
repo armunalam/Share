efccc8bc739a51decf391454690ae82eb37c6bee
armunalam <37847999+armunalam@users.noreply.github.com>
2019-09-26 10:35:49 +0600

    Create Insertion Sort.cpp

---
+++ Insertion Sort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void insertionSort(int A[], int size)
+{
+	int key, i;
+	for (int j = 2; j < size; j++)
+	{
+		key = A[j];
+		i = j - 1;
+		while (i >= 0 && A[i] > key)
+		{
+			A[i + 1] = A[i];
+			i--;
+		}
+		A[i + 1] = key;
+	}
+}
+
+int main()
+{
+	int size = 20;
+	int A[size];
+	for (int i = 0; i < size; i++)
+		{
+			A[i] = rand() % 100;
+		}
+	insertionSort(A, size);
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		cout << A[i] << ", ";
+	}
+	cout << A[size - 1];
+}
+
+
+
+
