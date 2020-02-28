e1d9f222b06a3b9b575b10919ba95782e5559438
armunalam <37847999+armunalam@users.noreply.github.com>
2020-02-28 18:54:34 +0600

    Create Quicksort.cpp

---
+++ Algorithms/Quicksort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int partition(int A[], int p, int r)
+{
+    int x = A[r];
+    int i = p - 1;
+
+    for (int j = p; j < r; j++)
+    {
+        if (A[j] < x)
+        {
+            ++i;
+            swap(A[i], A[j]);
+        }
+    }
+    swap(A[i + 1], A[r]);
+    return i + 1;
+}
+
+void quicksort(int A[], int p, int r)
+{
+    if (p < r)
+    {
+        int q = partition(A, p, r);
+        quicksort(A, p, q - 1);
+        quicksort(A, q + 1, r);
+    }
+}
+
+int main()
+{
+    int A[] = {5,4,8,7,6,2,3,4,5,6,8};
+    int n = sizeof(A) / sizeof(A[0]);
+
+    quicksort(A, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+        cout << A[i] << ' ';
+}
